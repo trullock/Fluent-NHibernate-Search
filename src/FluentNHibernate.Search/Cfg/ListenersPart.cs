277618060970dfe76b196c332e8ce5bbbe01bbfc
ybesson <yb@inetux.net>
2010-03-15 17:26:19 +0100

    Fixed method name

--- src/FluentNHibernate.Search/Cfg/ListenersPart.cs
+++ src/FluentNHibernate.Search/Cfg/ListenersPart.cs
@@ -3,59 +3,62 @@ using NHibernate.Search.Event;
 
 namespace FluentNHibernate.Search.Cfg
 {
-	public class ListenersPart
-	{
-		private readonly IFluentSearchConfiguration cfg;
+    public class ListenersPart
+    {
+        private readonly IFluentSearchConfiguration cfg;
 
-		public ListenersPart(IFluentSearchConfiguration cfg)
-		{
-			this.cfg = cfg;
-		}
+        public ListenersPart(IFluentSearchConfiguration cfg)
+        {
+            this.cfg = cfg;
+        }
 
-		public ListenersPart Default()
-		{
-			this.PostDelete<FullTextIndexEventListener>();
-			this.PostUpdate<FullTextIndexEventListener>();
-			this.PostInsert<FullTextIndexEventListener>();
+        public ListenersPart Default()
+        {
+            PostDelete<FullTextIndexEventListener>();
+            PostUpdate<FullTextIndexEventListener>();
+            PostInsert<FullTextIndexEventListener>();
 
-			return this;
-		}
+            return this;
+        }
 
-		public ListenersPart PostInsert<TEventListener>() where TEventListener : IPostInsertEventListener, new()
-		{
-			return this.PostInsert(new TEventListener());
-		}
+        public ListenersPart PostInsert<TEventListener>() where TEventListener : IPostInsertEventListener, new()
+        {
+            return PostInsert(new TEventListener());
+        }
 
-		public ListenersPart PostInsert<TEventListener>(TEventListener listener) where TEventListener : IPostInsertEventListener
-		{
-			return this.setListener(ListenerType.PostInsert, listener);
-		}
+        public ListenersPart PostInsert<TEventListener>(TEventListener listener)
+            where TEventListener : IPostInsertEventListener
+        {
+            return SetListener(ListenerType.PostInsert, listener);
+        }
 
-		public ListenersPart PostUpdate<TEventListener>() where TEventListener : IPostUpdateEventListener, new()
-		{
-			return this.PostUpdate(new TEventListener());
-		}
+        public ListenersPart PostUpdate<TEventListener>() where TEventListener : IPostUpdateEventListener, new()
+        {
+            return PostUpdate(new TEventListener());
+        }
 
-		public ListenersPart PostUpdate<TEventListener>(TEventListener listener) where TEventListener : IPostUpdateEventListener
-		{
-			return this.setListener(ListenerType.PostUpdate, listener);
-		}
+        public ListenersPart PostUpdate<TEventListener>(TEventListener listener)
+            where TEventListener : IPostUpdateEventListener
+        {
+            return SetListener(ListenerType.PostUpdate, listener);
+        }
 
-		public ListenersPart PostDelete<TEventListener>() where TEventListener : IPostDeleteEventListener, new()
-		{
-			return this.PostDelete(new TEventListener());
-		}
+        public ListenersPart PostDelete<TEventListener>() where TEventListener : IPostDeleteEventListener, new()
+        {
+            return PostDelete(new TEventListener());
+        }
 
-		public ListenersPart PostDelete<TEventListener>(TEventListener listener) where TEventListener : IPostDeleteEventListener
-		{
-			return this.setListener(ListenerType.PostDelete, listener);
-		}
+        public ListenersPart PostDelete<TEventListener>(TEventListener listener)
+            where TEventListener : IPostDeleteEventListener
+        {
+            return SetListener(ListenerType.PostDelete, listener);
+        }
 
 
-		private ListenersPart setListener(ListenerType type, object listener)
-		{
-			cfg.Configuration.SetListener(type, listener);
-			return this;
-		}
-	}
+        private ListenersPart SetListener(ListenerType type, object listener)
+        {
+            cfg.Configuration.SetListener(type, listener);
+            return this;
+        }
+    }
 }
